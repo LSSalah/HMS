3235a0a87152cdf8e61265df7688f630f601b0fe
Salah <74099821+LSSalah@users.noreply.github.com>
2021-01-22 11:49:34 +0200

    Update UserRepository.java

--- src/main/java/com/exam/show/repositories/UserRepository.java
+++ src/main/java/com/exam/show/repositories/UserRepository.java
@@ -14,13 +14,10 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	
 	
 	 User findByEmail(String email);
-<<<<<<< HEAD
 	
 	 User findByConfirmationToken(String confirmationToken);
 	 
 	  List<User> findAll();
-=======
 	 User findByConfirmationToken(String confirmationToken);
 	 List<User> findAll();
->>>>>>> f69d95e14609bee378bb9f215b53fd61934d0b98
 }
