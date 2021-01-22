95e51d1dc611fbeb612f173c1e0baa0fd350cb21
OsaidM <Osaid.m1@gmail.com>
2021-01-23 01:10:26 +0200

    stable v0.4

--- src/main/java/com/exam/show/controllers/UserController.java
+++ src/main/java/com/exam/show/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.exam.show.controllers;
 
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -78,6 +80,59 @@ public class UserController {
 		
 		return "user/index";
 	}
+	
+	@GetMapping("/profile")
+	public String profile(Model model){
+		
+		// get last seen
+		String username="";
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+		   username = ((UserDetails)principal).getUsername();
+		  String Pass = ((UserDetails)principal).getPassword();
+		  System.out.println("One + "+username+"   "+Pass);
+		  
+		  
+		} else {
+		 username = principal.toString();
+		  System.out.println("Two + "+username);
+		}
+		
+		Admin admin = adminServiceImplementation.findByEmail(username);
+				 
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+		    Date now = new Date();  
+		 
+		 List<Appointment> list = appointmentServiceImplementation.findAll();
+		 List<Appointment> newList = new ArrayList<>();
+		         
+		 for (Appointment appointment : list) {
+			 if(appointment.getEmail().equals(admin.getEmail())) {
+				 newList.add(appointment);
+			 }
+		}
+		 Appointment obj=new Appointment();
+		 
+		 obj.setName(admin.getFirstName()+" "+admin.getLastName());
+		 
+		 obj.setEmail(admin.getEmail());
+			
+		 System.out.println(obj);
+		 
+		 model.addAttribute("app",obj);
+		 model.addAttribute("profile",admin);
+		 model.addAttribute("appList",newList);
+		return "user/profile";
+	}
+	
+	@PostMapping("/update-profile")
+	public String updateProfile(@ModelAttribute("app") Admin admin) {
+		
+		adminServiceImplementation.save(admin);
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/user/profile";
+	}
+
 	
 	@PostMapping("/save-app")
 	public String saveEmploye(@ModelAttribute("app") Appointment obj) {
